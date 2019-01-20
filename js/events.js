7819da6fdf324a75fdfe4c088f7840fad0a8dfb5
Kevin Ngo <k.ngo93@outlook.com>
2019-01-20 22:52:38 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keypress', function(key){
+  $('form').on('keydown', function(key){
     if (key.which == 71) {
       alert ("G was pressed");
       return;
