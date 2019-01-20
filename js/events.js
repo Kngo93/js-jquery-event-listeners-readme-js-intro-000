01844986adfdd6fb0bd644aa4e3e216ad78e44b1
Kevin Ngo <k.ngo93@outlook.com>
2019-01-20 22:51:14 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,17 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $('form').on('keypress', function(key){
+    if (key.which == 103) {
+      alert ("G was pressed");
+      return;
+    } else {
+      return;
+    }
+
+  });
+}
 $(document).ready(function(){
 
 // call functions here
